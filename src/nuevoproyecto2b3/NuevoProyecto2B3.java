d84bb624c19b598a20caa1fa5256d2696e06c6ee
danieljaimes <danieljaimes@192.168.0.8>
2021-02-25 19:43:33 -0600

    y

---
+++ src/nuevoproyecto2b3/NuevoProyecto2B3.java
@@ -0,0 +1,22 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package nuevoproyecto2b3;
+
+/**
+ *
+ * @author danieljaimes
+ */
+public class NuevoProyecto2B3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("prueba2");
+    }
+    
+}
