63242662e6853302b337adbd9a3d3f65913410e2
danieljaimes <danieljaimes@192.168.0.8>
2021-02-25 19:46:55 -0600

    2do

--- src/nuevoproyecto2b3/NuevoProyecto2B3.java
+++ src/nuevoproyecto2b3/NuevoProyecto2B3.java
@@ -17,6 +17,8 @@ public class NuevoProyecto2B3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("prueba2");
+        
+        int edad = 21;
     }
     
 }
